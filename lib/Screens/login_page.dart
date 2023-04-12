b882928bf7532c9476fdf8fec7ff8c3dda251937
Naima ELJID <114741266+neimael@users.noreply.github.com>
2023-04-12 00:01:49 +0000

    splash screen

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:attend_me/Screens/forgot_password.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -19,166 +20,194 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 45),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image.asset("assets/loginQR.png"),
-                const Gap(5),
-                const Text(
-                  "Welcome !",
-                  style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF6096B4),
-                  ),
+      body: Stack(
+        children: [
+          Positioned(
+            right: -50,
+            top: -120,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color(0xFF6096B4),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 200,
+                  color: const Color(0xFF6096B4),
                 ),
-                const Gap(20),
-                TextFormField(
-                  onTap: () {
-                    setState(() {
-                      _isTappedEmail = true;
-                      _isTappedPass = false;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    labelStyle: TextStyle(
-                      color: _isTappedEmail
-                          ? const Color(0xFF6096B4)
-                          : const Color.fromARGB(255, 112, 112, 112),
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 45),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("assets/loginQR.png"),
+                    const Gap(3),
+                    const Text(
+                      "Welcome !",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
                         color: Color(0xFF6096B4),
-                        width: 2.0,
-                        style: BorderStyle.solid,
                       ),
                     ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xFF6096B4),
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.email,
-                      color: Color(0xFF6096B4),
-                    ),
-                  ),
-                ),
-                const Gap(15),
-                TextFormField(
-                  onTap: () {
-                    setState(() {
-                      _isTappedPass = true;
-                      _isTappedEmail = false;
-                    });
-                  },
-                  obscureText: _hideText,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    labelStyle: TextStyle(
-                      color: _isTappedPass
-                          ? const Color(0xFF6096B4)
-                          : const Color.fromARGB(255, 112, 112, 112),
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xFF6096B4),
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xFF6096B4),
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _hideText ? Icons.visibility_off : Icons.visibility,
-                        color: const Color(0xFF6096B4),
-                      ),
-                      onPressed: () {
+                    const Gap(20),
+                    TextFormField(
+                      onTap: () {
                         setState(() {
-                          _hideText = !_hideText;
+                          _isTappedEmail = true;
+                          _isTappedPass = false;
                         });
                       },
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                      color: Color(0xFF6096B4),
-                    ),
-                  ),
-                ),
-                const Gap(25),
-                Row(
-                  children: [
-                    Checkbox(
-                      activeColor: const Color(0xFF6096B4),
-                      value: _rememberMe,
-                      onChanged: (value) {
-                        setState(
-                          () {
-                            _rememberMe = value!;
-                          },
-                        );
-                      },
-                    ),
-                    const Expanded(
-                      child: Text(
-                        "Remember Me",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 148, 148, 148),
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: _isTappedEmail
+                              ? const Color(0xFF6096B4)
+                              : const Color.fromARGB(255, 112, 112, 112),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF6096B4),
+                            width: 2.0,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF6096B4),
+                            width: 2.0,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Color(0xFF6096B4),
                         ),
                       ),
                     ),
-                    const Expanded(
-                      child: Text(
-                        "Forgot password ?",
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 148, 148, 148),
-                          fontSize: 14,
+                    const Gap(15),
+                    TextFormField(
+                      onTap: () {
+                        setState(() {
+                          _isTappedPass = true;
+                          _isTappedEmail = false;
+                        });
+                      },
+                      obscureText: _hideText,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: _isTappedPass
+                              ? const Color(0xFF6096B4)
+                              : const Color.fromARGB(255, 112, 112, 112),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF6096B4),
+                            width: 2.0,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF6096B4),
+                            width: 2.0,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _hideText ? Icons.visibility_off : Icons.visibility,
+                            color: const Color(0xFF6096B4),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _hideText = !_hideText;
+                            });
+                          },
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Color(0xFF6096B4),
+                        ),
+                      ),
+                    ),
+                    const Gap(15),
+                    Row(
+                      children: [
+                        Checkbox(
+                          activeColor: const Color(0xFF6096B4),
+                          value: _rememberMe,
+                          onChanged: (value) {
+                            setState(
+                              () {
+                                _rememberMe = value!;
+                              },
+                            );
+                          },
+                        ),
+                        const Expanded(
+                          child: Text(
+                            "Remember Me",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 148, 148, 148),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ForgotPassPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Forgot password ?",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Color.fromARGB(255, 148, 148, 148),
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Gap(20),
+                    SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF6096B4),
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(35))),
+                        onPressed: () {},
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
-                const Gap(40),
-                SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF6096B4),
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(35))),
-                    onPressed: () {},
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
---
+++ lib/Screens/welcome_page.dart
@@ -0,0 +1,96 @@
+import 'package:attend_me/Screens/login_page.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  /*@override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF6096B4),
+      body: Stack(
+        children: [
+          Positioned(
+            left: -163,
+            top: -160,
+            child: Container(
+              padding: const EdgeInsets.all(164),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 5,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -180,
+            top: -180,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 145,
+                  color: Colors.transparent,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -171,
+            bottom: -171,
+            child: Container(
+              padding: const EdgeInsets.all(164),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 5,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -190,
+            bottom: -190,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 145,
+                  color: Colors.transparent,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Image.asset("assets/AttendMeS.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
