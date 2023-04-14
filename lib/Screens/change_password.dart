d2edebbefb96ed776a9d871746738d5d5899d7ba
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-04-14 02:59:12 +0000

    some changes

--- lib/Screens/change_password.dart
+++ lib/Screens/change_password.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ChangePasswordPage extends StatefulWidget {
   const ChangePasswordPage({super.key});
@@ -8,8 +9,96 @@ class ChangePasswordPage extends StatefulWidget {
 }
 
 class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Gap(60),
+                const Text(
+                  "Verification Code !  ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 110, 110, 110),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Gap(20),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 25),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          child: Container(
+                            padding: const EdgeInsets.all(30),
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF6096B4),
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 100,
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 20),
+                            child: Container(
+                                height: 210,
+                                width: 210,
+                                child: Center(
+                                    child: Image.asset(
+                                        "assets/verification.png"))),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Gap(20),
+                Column(
+                  children: const [
+                    Text(
+                      "Please enter the four 4 code sent to  ",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 88, 84, 84),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      " 06 25 67 89 09 ",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 88, 84, 84),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        )
+        )
+            
+    );
   }
 }
