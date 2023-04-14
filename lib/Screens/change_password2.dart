5aa9da5b86f316c1f5b2859e22b92499f5513c68
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-04-14 17:37:21 +0000

    some changes

---
+++ lib/Screens/change_password2.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:attend_me/Screens/profile_page.dart';
+
+class changePassword2 extends StatefulWidget {
+  const changePassword2({super.key});
+
+  @override
+  State<changePassword2> createState() => _changePassword2State();
+}
+
+class _changePassword2State extends State<changePassword2> {
+ final formKey = GlobalKey<FormState>();
+  bool _isTappedPass = false;
+  bool _hideText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Gap(60),
+                const Text(
+                  "Change your password ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 110, 110, 110),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Gap(5),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 25),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          child: Container(
+                            padding: const EdgeInsets.all(30),
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF6096B4),
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 100,
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 20),
+                            child: Container(
+                                height: 210,
+                                width: 210,
+                                child: Center(
+                                    child: Image.asset(
+                                        "assets/newPass.png"))),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Gap(1),
+                Column(
+                  children: const [
+                    Text(
+                      " Your new password must be different from previously used password",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 163, 158, 158),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                   
+                  ],
+                ),
+                 Gap(20),
+Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: TextFormField(
+                    onTap: () {
+                      setState(() {
+                        _isTappedPass = true;
+                        
+                      });
+                    },
+                    obscureText: _hideText,
+                    decoration: InputDecoration(
+                      labelText: " Old Password",
+                      labelStyle: TextStyle(
+                        color: _isTappedPass
+                            ? const Color(0xFF6096B4)
+                            : const Color.fromARGB(255, 112, 112, 112),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _hideText ? Icons.visibility_off : Icons.visibility,
+                          color: const Color(0xFF6096B4),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _hideText = !_hideText;
+                          });
+                        },
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color(0xFF6096B4),
+                      ),
+                    ),
+                  ),
+                ),
+                  Gap(25),
+Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: TextFormField(
+                    onTap: () {
+                      setState(() {
+                        _isTappedPass = true;
+                        
+                      });
+                    },
+                    obscureText: _hideText,
+                    decoration: InputDecoration(
+                      labelText: "New Password",
+                      labelStyle: TextStyle(
+                        color: _isTappedPass
+                            ? const Color(0xFF6096B4)
+                            : const Color.fromARGB(255, 112, 112, 112),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _hideText ? Icons.visibility_off : Icons.visibility,
+                          color: const Color(0xFF6096B4),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _hideText = !_hideText;
+                          });
+                        },
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color(0xFF6096B4),
+                      ),
+                    ),
+                  ),
+                ),
+                                  Gap(20),
+Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: TextFormField(
+                    onTap: () {
+                      setState(() {
+                        _isTappedPass = true;
+                        
+                      });
+                    },
+                    obscureText: _hideText,
+                    decoration: InputDecoration(
+                      labelText: " Confirm New Password",
+                      labelStyle: TextStyle(
+                        color: _isTappedPass
+                            ? const Color(0xFF6096B4)
+                            : const Color.fromARGB(255, 112, 112, 112),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF6096B4),
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _hideText ? Icons.visibility_off : Icons.visibility,
+                          color: const Color(0xFF6096B4),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _hideText = !_hideText;
+                          });
+                        },
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.lock,
+                        color: Color(0xFF6096B4),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                
+                SizedBox(
+                  height: 35,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF6096B4),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(35))),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfilePage()),
+                      );
+                    },
+                    child: const Text(
+                      "Save",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            
+          ),
+        )
+          
+        )
+            
+    );
+  }
+}
