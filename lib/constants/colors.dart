f8440a60604db81b55079b29576bea20a03f0979
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-04-16 23:13:52 +0000

    some changes

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color darkBlue = Color.fromARGB(255, 24, 94, 133);
+Color lightBlue =Color.fromARGB(255, 144, 191, 216);
+Color maincolor = Color.fromARGB(255, 78, 146, 163);
