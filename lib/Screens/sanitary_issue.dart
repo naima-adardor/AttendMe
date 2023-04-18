226cffcdfe283bd90a6d10ba3c8d35d745fce39d
Naima ELJID <114741266+neimael@users.noreply.github.com>
2023-04-18 20:16:42 +0000

    Sanitary issues page

--- lib/Screens/sanitary_issue.dart
+++ lib/Screens/sanitary_issue.dart
@@ -1,4 +1,6 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SanitaryPage extends StatefulWidget {
   const SanitaryPage({super.key});
@@ -8,8 +10,183 @@ class SanitaryPage extends StatefulWidget {
 }
 
 class _SanitaryPageState extends State<SanitaryPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 144, 191, 216),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 50),
+          child: Text(
+            'Sanitary Issues',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 144, 191, 216),
+      body: Column(
+        children: [
+          Form(
+            key: formKey,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 50,
+                  bottom: 50,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 320,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color.fromARGB(195, 255, 255, 255),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 158, 158, 158)
+                                .withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 3,
+                          ),
+                          child: TextFormField(
+                            onTap: () {
+                              setState(() {});
+                            },
+                            maxLines: null,
+                            decoration: const InputDecoration(
+                              hintText: "Write Your Report Here !",
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(255, 124, 124, 124),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        right: 80,
+                      ),
+                      child: Text(
+                        "Upload Your medical certificte",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 66, 66, 66),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    const Gap(20),
+                    Container(
+                      height: 60,
+                      width: 320,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color.fromARGB(195, 255, 255, 255),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 158, 158, 158)
+                                .withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 3,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "No file selected !",
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 124, 124, 124),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red,
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(35))),
+                                onPressed: () {},
+                                child: const Text(
+                                  "Upload",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(30),
+                    SizedBox(
+                      height: 56,
+                      width: 180,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF6096B4),
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(35))),
+                        onPressed: () {},
+                        child: const Text(
+                          "Submit",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
